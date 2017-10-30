1b211912544c46bd0e3514819dcc6e376c28a781
StinkyBrownApple <33228295+StinkyBrownApple@users.noreply.github.com>
2017-10-30 16:22:45 +0000

    Added files

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    [SerializeField]
+    Sprite tileSprite;
+    [SerializeField]
+    Sprite bombSprite;
+    [SerializeField]
+    Sprite flagSprite;
+    [SerializeField]
+    Sprite safeSprite;
+    [SerializeField]
+    Sprite oneSprite;
+    [SerializeField]
+    Sprite twoSprite;
+    [SerializeField]
+    Sprite threeSprite;
+    [SerializeField]
+    Sprite fourSprite;
+    [SerializeField]
+    Sprite fiveSprite;
+    [SerializeField]
+    Sprite sixSprite;
+    [SerializeField]
+    Sprite sevenSprite;
+    [SerializeField]
+    Sprite eightSprite;
+
+    TileManager manager;
+
+    bool isFlag = false;
+    bool clicked = false;
+
+    public enum TileType
+    {
+        None,
+        Number,
+        Bomb,
+    };
+
+    private TileType type = TileType.None;
+
+    public TileType Type
+    {
+        get
+        {
+            return type;
+        }
+
+        set
+        {
+            type = value;
+        }
+    }
+
+    public void Click(bool rightClick = false)
+    {
+        if (clicked)
+            return;
+
+
+        if (isFlag && !rightClick)
+            return;
+
+        if (isFlag && rightClick)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = tileSprite;
+            isFlag = false;
+            return;
+        }
+
+        if (!isFlag && rightClick)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = flagSprite;
+            isFlag = true;
+            return;
+        }
+
+        clicked = true;
+
+        if (manager == null)
+            manager = GetComponentInParent<TileManager>();
+
+        if (type == TileType.Bomb)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = bombSprite;
+        }
+
+        if (type == TileType.Number)
+        {
+            int surroundingBombs = 0;
+
+            Vector2 posCheck;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    posCheck = new Vector2(transform.position.x + x, transform.position.y + y);
+
+                    if (manager.tileTable.ContainsKey(posCheck))
+                    {
+                        if (manager.tileTable[posCheck].Type == TileType.Bomb)
+                        {
+                            surroundingBombs++;
+                        }
+                    }
+                }
+            }
+            switch (surroundingBombs)
+            {
+                case 0:
+                    transform.GetComponent<SpriteRenderer>().sprite = safeSprite;
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        for (int y = -1; y <= 1; y++)
+                        {
+                            if (x == 0 && y == 0)
+                                continue;
+
+                            posCheck = new Vector2(transform.position.x + x, transform.position.y + y);
+
+                            if (manager.tileTable.ContainsKey(posCheck))
+                            {
+                                manager.tileTable[posCheck].Click();
+                            }
+                        }
+                    }
+                    break;
+                case 1:
+                    transform.GetComponent<SpriteRenderer>().sprite = oneSprite;
+                    break;
+                case 2:
+                    transform.GetComponent<SpriteRenderer>().sprite = twoSprite;
+                    break;
+                case 3:
+                    transform.GetComponent<SpriteRenderer>().sprite = threeSprite;
+                    break;
+                case 4:
+                    transform.GetComponent<SpriteRenderer>().sprite = fourSprite;
+                    break;
+                case 5:
+                    transform.GetComponent<SpriteRenderer>().sprite = fiveSprite;
+                    break;
+                case 6:
+                    transform.GetComponent<SpriteRenderer>().sprite = sixSprite;
+                    break;
+                case 7:
+                    transform.GetComponent<SpriteRenderer>().sprite = sevenSprite;
+                    break;
+                case 8:
+                    transform.GetComponent<SpriteRenderer>().sprite = eightSprite;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject tile;
+    [SerializeField]
+    Camera cam;
+    Vector2 screenSize;
+    public Dictionary<Vector2, Tile> tileTable = new Dictionary<Vector2, Tile>();
+    int bombCount = 20;
+    Vector2 mousePos;
+
+    // Use this for initialization
+    void Start()
+    {
+        screenSize = cam.ViewportToWorldPoint(new Vector2(1, 1));
+        screenSize.x = (int)screenSize.x;
+        screenSize.y = (int)screenSize.y;
+        GenerateTiles();
+        SetTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        mousePos.x = (int)mousePos.x;
+        mousePos.y = (int)mousePos.y;
+        if(Input.GetMouseButtonUp(0))
+        {
+            tileTable[mousePos].Click();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            tileTable[mousePos].Click(true);
+        }
+    }
+
+    private void GenerateTiles()
+    {
+        for (int x = 0; x < screenSize.x; x++)
+        {
+            for (int y = 0; y < screenSize.y; y++)
+            {
+                tileTable.Add(new Vector2(x, y), Instantiate(tile, new Vector2(x, y), Quaternion.identity, transform).GetComponent<Tile>());
+            }
+        }
+    }
+
+    private void SetTiles()
+    {
+        Vector2 randPos;
+        for (int i = 0; i < bombCount; i++)
+        {
+            do
+            {
+                randPos = new Vector2((int)Random.Range(0, screenSize.x), (int)Random.Range(0, screenSize.y));
+            }
+            while (tileTable[randPos].Type == Tile.TileType.Bomb);
+
+            tileTable[randPos].Type = Tile.TileType.Bomb;
+        }
+
+        Vector2 pos;
+        for (int x = 0; x < screenSize.x; x++)
+        {
+            for (int y = 0; y < screenSize.y; y++)
+            {
+                pos = new Vector2(x, y);
+                if (tileTable[pos].Type != Tile.TileType.Bomb)
+                    tileTable[pos].Type = Tile.TileType.Number;
+            }
+        }
+    }
+}
